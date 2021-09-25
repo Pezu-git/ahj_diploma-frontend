1f135f4b126b0dc339ecd1e77effca2818d48021
Pezu-git <artem_nikolaev69@mail.ru>
2021-09-25 12:04:27 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+import API from './API';
+import Controller from './Controller';
+import Layout from './Layout';
+import SVG from './svg';
+
+const app = new Controller(new Layout(new SVG()), new API());
+
+app.init();
